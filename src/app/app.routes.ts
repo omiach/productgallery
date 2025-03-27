b5d6a31245e37978aff8cd4f19348acb062b5ea4
Oleksandr Miach <ovmiach@gmail.com>
2025-03-27 18:21:11 +0200

    save

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,15 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './components/home-page/home-page.component';
+import { ProductSearchComponent } from './components/product-search/product-search.component';
+import { MainComponent } from './components/main/main.component';
 
-export const routes: Routes = [{ path: '', component: HomePageComponent }];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent,
+    children: [
+      { path: '', component: HomePageComponent },
+      { path: 'productssearch', component: ProductSearchComponent },
+    ],
+  },
+];
--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { WishlistComponent } from '../wishlist/wishlist.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home-page',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, WishlistComponent, CommonModule],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
 })
 export class HomePageComponent {
+  public showWishlist: boolean = true;
+
   toggleDarkTheme() {
     document.body.classList.toggle('dark-theme');
   }
